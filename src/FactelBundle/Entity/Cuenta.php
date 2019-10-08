b104009f1873ee7181ae8da4b529b6f2bccc88b4
xalava <xalava@stb.com>
2019-10-08 15:11:59 -0500

    segunda op

--- src/FactelBundle/Entity/Cuenta.php
+++
@@ -1,5 +0,0 @@
-<?php
-namespace FactelBundle\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
-use Knp\DoctrineBehaviors\Model as ORMBehaviors;
--- src/FactelBundle/FactelBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FactelBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class FactelBundle extends Bundle
-{
-}
